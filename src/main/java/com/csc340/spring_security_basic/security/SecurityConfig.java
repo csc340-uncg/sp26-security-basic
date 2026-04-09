f25a60519452d0969e5be0440c4bcc39be58a240
csc340-uncg <uncg.csc340@gmail.com>
2026-04-08 21:25:37 -0400

    Implement initial Spring Security setup with user roles and access control

---
+++ src/main/java/com/csc340/spring_security_basic/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.csc340.spring_security_basic.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  private AppUserDetailsService userDetailsService;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/", "/home").permitAll()
+            .requestMatchers("/mod/**").hasAnyRole("KNIGHT", "MASTER")
+            .requestMatchers("/admin/**").hasAnyRole("MASTER")
+            .anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults())
+        .exceptionHandling((x) -> x.accessDeniedPage("/403"))
+        .logout(Customizer.withDefaults());
+    return http.build();
+  }
+
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService);
+  }
+
+}
